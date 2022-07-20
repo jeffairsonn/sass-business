5130c27c7ad53fe6c5979aa0fbdfae51a4d7ef55
jeffairsonn <thomasbelenuspro@gmail.com>
2022-07-20 16:34:55 +0200

    try to connect to the api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import GeneratedImage from './components/GeneratedImage';
+import GlobalGeneratedStats from './components/GlobalGeneratedStats';
 import Heading from './components/heading';
 import UserList from './components/UserList';
 
 const Home = () => {
     return (
         <Heading>
+            <GlobalGeneratedStats />
             <UserList />
         </Heading>
     );
